2be7185c8d85b06fbcd52ddc2d39b660d781c516
lucassoaresfr <lucasclachack@gmail.com>
2026-09-03 10:14:15 -0300

    Hotfix: Correção do basePath da web

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.79", "painelcomal.duckdns.org"],
+  basePath: "/winthor-ia",
+  output: "standalone",
+  assetPrefix: "/recep-ped",
 };
 
 export default nextConfig;
