55b1a4a199d10892fa2154bbdfe8c2061825b92c
lucassoaresfr <lucasclachack@gmail.com>
2026-09-16 11:32:26 -0300

    hotfix devops

--- apps/web/src/service/connect.ts
+++ apps/web/src/service/connect.ts
@@ -3,19 +3,19 @@ import axios, { AxiosInstance } from "axios";
 
 export const connectIA = () => {
   const api = axios.create({
-    baseURL: "https://painelcomal.duckdns.org/dev-test/backend/api/v1",
+    // baseURL: "https://painelcomal.duckdns.org/dev-test/backend/api/v1",
+    baseURL: "https://painelcomal.duckdns.org/dev-test/winthor-ia/backend/api/v1",
     // baseURL: "http://localhost:8081/api/v1",
   });
 
   return api;
 };
 
-
 export const connectDB = (): AxiosInstance => {
   const instance = axios.create({
     // baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8080/api/v1",
-    // baseURL: "https://painelcomal.duckdns.org/winthor-ia/api/api/v1"
-    baseURL: "https://painelcomal.duckdns.org/dev-test/api/api/v1"
+    baseURL: "https://painelcomal.duckdns.org/winthor-ia/api/api/v1",
+    // baseURL: "https://painelcomal.duckdns.org/dev-test/api/api/v1"
   });
 
   return instance;
